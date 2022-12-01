e569ccdb9aa12a4034391aa9768aac557c8ca9dc
mbm_1607 <muhammadkhan1607@gmail.com>
2022-12-01 14:36:52 +0500

    Init express & replace nodemon with watch mode

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const port = 5000;
+
+app.listen(
+  port,
+  () => console.log(`Server listening at http://localhost:${port}`)
+);
+
+app.get('/', (req, res) => {
+  return 'Hello, Express';
+});
